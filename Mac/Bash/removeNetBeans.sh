3009db4b3d64bf964a2775a5b454c14ac1b74819
Kevin Baynes <kevin@paatusa.com>
2018-11-16 09:47:48 -0500

    Add script to remove NetBeans from Mac

---
+++ Mac/Bash/removeNetBeans.sh
@@ -0,0 +1,12 @@
+# Removes NetBeans installation from Mac
+# kbaynes
+# Nov 2018
+# Works with NB 8
+
+# Call this script as Root
+# sudo removeNetBeans.sh
+
+rm -rfv ~/Application/Netbeans
+rm -rfv ~/Library/Application\ Support/NetBeans/
+rm -rfv ~/Library/Caches/NetBeans/
+rm -rfv /private/var/db/receipts/org.netbeans.*
